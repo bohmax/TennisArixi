7826137f06e9598af7e16022b3d0714875a5b3ac
bohmax <mssmpuddu@gmail.com>
2021-10-15 23:38:29 +0200

    updated

--- Scripts/gh-pages-deploy.js
+++ Scripts/gh-pages-deploy.js
@@ -3,6 +3,7 @@ const execa = require("execa");
 const fs = require("fs");
 (async () => {
     try {
+        await execa("git", ["commit", "-m", "emergency"]);
         await execa("git", ["checkout", "--orphan", "gh-pages"]);
         // eslint-disable-next-line no-console
         console.log("Building started...");

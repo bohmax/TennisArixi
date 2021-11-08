5b5e349db13d266d38885a5d8f6010682add9224
Massimo Puddu <mssmpuddu@gmail.com>
2021-11-08 23:27:47 +0100

    gh-pages

---
+++ precache-manifest.e045431a53cf4708c78b5754c2956047.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d9afa8e617d2b0224d5",
+    "url": "/TennisArixi/css/app.a5a47bb6.css"
+  },
+  {
+    "revision": "04a7cf47c7b5921edc53",
+    "url": "/TennisArixi/css/chunk-vendors.f8ec0789.css"
+  },
+  {
+    "revision": "06f93e69eb084550d1a7aaba3a8b26ec",
+    "url": "/TennisArixi/img/logo.06f93e69.png"
+  },
+  {
+    "revision": "f7a307cce857ffb5977a34ed8f380d95",
+    "url": "/TennisArixi/index.html"
+  },
+  {
+    "revision": "8d9afa8e617d2b0224d5",
+    "url": "/TennisArixi/js/app.24c975ee.js"
+  },
+  {
+    "revision": "04a7cf47c7b5921edc53",
+    "url": "/TennisArixi/js/chunk-vendors.644dcb90.js"
+  },
+  {
+    "revision": "aa5117f2ee08b615d8f9258a09bea5a9",
+    "url": "/TennisArixi/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/TennisArixi/robots.txt"
+  }
+]);
